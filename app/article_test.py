02cc08b88335512f02c4fa52800e911a24b192e0
odipojames <odipojames12@gmai.com>
2018-11-09 17:20:03 +0300

    add test for article

---
+++ app/article_test.py
@@ -0,0 +1,32 @@
+import unittest
+from models import article
+
+Article = article.Article
+
+
+class ArticleTest(unittest.TestCase):
+    '''
+    testing the behaviour of article
+    '''
+    def setUp(self):
+        '''
+        runs before each test
+        '''
+        self.new_article = Article('a','b','c','d','e','f')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+
+    def test_init(self):
+        '''
+        testing whether the instances of article are started correctly
+        '''
+        self.assertEqual(self.new_article.author,'a')
+        self.assertEqual(self.new_article.title, 'b')
+        self.assertEqual(self.new_article.description, 'c')
+        self.assertEqual(self.new_article.url, 'd')
+        self.assertEqual(self.new_article.image, 'e')
+        self.assertEqual(self.new_article.time, 'f')
+
+if __name__ == '__main__':
+    unittest.main()
